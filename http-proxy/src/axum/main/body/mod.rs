f88d5338d64ac6827f41b1fdd6e686bf30265ead
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2023-09-19 15:04:33 +0200

    :(

---
+++ http-proxy/src/axum/main/body/mod.rs
@@ -0,0 +1,10 @@
+//! HTTP body utilities.
+
+#[doc(no_inline)]
+pub use http_body::Body as HttpBody;
+
+#[doc(no_inline)]
+pub use bytes::Bytes;
+
+#[doc(inline)]
+pub use axum_core::body::Body;
---
+++ http-proxy/src/bidirectional/io/mod.rs
@@ -0,0 +1,7 @@
+mod async_read;
+mod async_write;
+mod read_buf;
+
+pub use async_read::AsyncRead;
+pub use async_write::AsyncWrite;
+pub use read_buf::ReadBuf;
