7e113637dbfd7ae83a6fdd92045254d9b5ef34d5
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2023-10-03 14:22:13 +0200

    centralizing reder function

--- plotters-yew/src/main.rs
+++ plotters-yew/src/main.rs
@@ -1,11 +1,9 @@
-use std::sync::Mutex;
+use web_sys::{
+    HtmlCanvasElement, HtmlDivElement, HtmlInputElement, HtmlLabelElement, HtmlSelectElement,
+};
+use yew::{html, Component, Context, Html, NodeRef};
 
-use web_sys::{Event, HtmlDivElement, HtmlInputElement, HtmlLabelElement, MouseEvent};
-use yew::{html, Callback, Component, Context, Html, NodeRef};
-
-use crate::canvas::{func_plot, mandelbrot, plot3d, Chart};
-
-use once_cell::sync::Lazy;
+use crate::canvas::{func_plot, mandelbrot, plot3d};
 
 mod canvas;
 mod types;
@@ -21,6 +19,13 @@ thread_local! {
     pub static MANDELBROT_CTRLS_REF: NodeRef = NodeRef::default();
     pub static ITERS_REF: NodeRef = NodeRef::default();
     pub static ITERS_LABEL_REF: NodeRef = NodeRef::default();
+    pub static STATUS_REF: NodeRef = NodeRef::default();
+}
+
+enum UpgradeType {
+    Graph(i32),
+    Mandelbrot(usize),
+    Plot3d(f64, f64),
 }
 
 #[derive(Clone)]
@@ -33,6 +38,7 @@ pub struct State {
     mandelbrot_ctrls_ref: NodeRef,
     iters_ref: NodeRef,
     iters_label_ref: NodeRef,
+    status_ref: NodeRef,
 }
 
 impl Component for App {
@@ -52,7 +58,7 @@ impl Component for App {
                     <h1>{"Plotters and Yew Demo"}</h1>
                     <div id={"coord"}></div>
                     <canvas id={"canvas"} width={"600"} height={"400"} ref={state.canvas_ref}></canvas>
-                    <div id={"status"}>{"Loading Plotters..."}</div>
+                    <div id={"status"} ref={state.status_ref}>{"Loading Plotters..."}</div>
                     <div id={"control"}>
                         <label for={"plot-type"}>{"Demo: "}</label>
                         <select id={"plot-type"} ref={state.select_ref} onchange={selection_callback}>
@@ -72,7 +78,7 @@ impl Component for App {
                             <input type={"range"} min={"0"} max={"314"} id={"yaw"} ref={state.yaw_ref} onchange={update_plot_3d} />
                         </div>
                         <div id={"mandelbrot-control"} hidden={true} ref={state.mandelbrot_ctrls_ref}>
-                            <label for={"iterations"} ref={state.iters_label_ref}>{"Iterations (50): "}</label>
+                            <label for={"iterations"} ref={state.iters_label_ref}>{"Iterations: "}</label>
                             <input type={"number"} min={"0"} id={"iterations"} ref={state.iters_ref} onchange={update_mandelbrot_iterators} /> <br />
                         </div>
                     </div>
@@ -81,7 +87,7 @@ impl Component for App {
                     <a href={"https://github.com/plotters-rs/plotters-wasm-demo"} target={"a"}>{"Source"}</a> { " | " }
                     <a href={"https://github.com/plotters-rs/plotters"} target={"a"}>{"Repo"}</a> { " | " }
                     <a href={"https://crates.io/crates/plotters"} target={"a"}>{"Crates"}</a> { " | " }
-                    <a href={"https://docs.rs/plotters"} target={"a"}>{"Docs"}</a> { " | " }
+                    <a href={"https://docs.rs/plotters"} target={"a"}>{"Docs"}</a>
                 </footer>
             </>
         }
@@ -99,14 +105,7 @@ fn update_mandelbrot_iterators<T>(_: T) {
         }
     };
 
-    let canvas = match state.canvas_ref.cast::<web_sys::HtmlCanvasElement>() {
-        Some(element) => element,
-        None => {
-            gloo::console::log!("No input was provided");
-            return;
-        }
-    };
-    let iters = match state.iters_ref.cast::<web_sys::HtmlInputElement>() {
+    let iters = match state.iters_ref.cast::<HtmlInputElement>() {
         Some(element) => element.value_as_number(),
         None => {
             gloo::console::log!("No input was provided");
@@ -114,19 +113,12 @@ fn update_mandelbrot_iterators<T>(_: T) {
         }
     };
     label.set_inner_text(&format!("Iterations: {}", iters as usize));
-    let _ = mandelbrot::draw(canvas, iters as usize);
+    update_plot(UpgradeType::Mandelbrot(iters as usize))
 }
 
 fn selection_callback<T>(_: T) {
     let state = get_state();
-    let select = match state.select_ref.cast::<web_sys::HtmlSelectElement>() {
-        Some(element) => element,
-        None => {
-            gloo::console::log!("No input was provided");
-            return;
-        }
-    };
-    let element = match state.canvas_ref.cast::<web_sys::HtmlCanvasElement>() {
+    let select = match state.select_ref.cast::<HtmlSelectElement>() {
         Some(element) => element,
         None => {
             gloo::console::log!("No input was provided");
@@ -134,17 +126,95 @@ fn selection_callback<T>(_: T) {
         }
     };
     let key = select.selected_index();
-    let threed_controls: HtmlDivElement =
-        match state.threed_ctrls_ref.cast::<web_sys::HtmlDivElement>() {
-            Some(element) => element,
-            None => {
-                gloo::console::log!("No input was provided");
-                return;
-            }
-        };
+
+    match key {
+        0 | 1 | 2 | 3 | 4 | 5 => update_plot(UpgradeType::Graph(key)),
+        6 => {
+            let iters = match state.iters_ref.cast::<HtmlInputElement>() {
+                Some(element) => element.value_as_number(),
+                None => {
+                    gloo::console::log!("No input was provided");
+                    return;
+                }
+            };
+            update_plot(UpgradeType::Mandelbrot(iters as usize))
+        }
+        7 => {
+            let pitch = match state.pitch_ref.cast::<HtmlInputElement>() {
+                Some(element) => element.value_as_number(),
+                None => {
+                    gloo::console::log!("No input was provided");
+                    return;
+                }
+            };
+            let yaw = match state.yaw_ref.cast::<HtmlInputElement>() {
+                Some(element) => element.value_as_number(),
+                None => {
+                    gloo::console::log!("No input was provided");
+                    return;
+                }
+            };
+            update_plot(UpgradeType::Plot3d(pitch, yaw))
+        }
+        _ => {}
+    }
+}
+
+fn update_plot_3d<T>(_: T) {
+    let window = web_sys::window().unwrap();
+    let performance = window.performance().unwrap();
+    let start_time = performance.now();
+    let state = get_state();
+    let pitch = match state.pitch_ref.cast::<HtmlInputElement>() {
+        Some(element) => element.value_as_number(),
+        None => {
+            gloo::console::log!("No input was provided");
+            return;
+        }
+    };
+    let yaw = match state.yaw_ref.cast::<HtmlInputElement>() {
+        Some(element) => element.value_as_number(),
+        None => {
+            gloo::console::log!("No input was provided");
+            return;
+        }
+    };
+    update_plot(UpgradeType::Plot3d(pitch, yaw));
+    let end_time = performance.now();
+    let duration = end_time - start_time;
+    let status = match state.status_ref.cast::<HtmlDivElement>() {
+        Some(element) => element,
+        None => {
+            gloo::console::log!("No input was provided");
+            return;
+        }
+    };
+    status.set_inner_text(&format!("Updating canvas took {}ms", duration))
+}
+
+fn update_plot(upgrade: UpgradeType) {
+    let window = web_sys::window().unwrap();
+    let performance = window.performance().unwrap();
+    let start_time = performance.now();
+    let state = get_state();
+
+    let canvas = match state.canvas_ref.cast::<HtmlCanvasElement>() {
+        Some(element) => element,
+        None => {
+            gloo::console::log!("No input was provided");
+            return;
+        }
+    };
+    let threed_controls: HtmlDivElement = match state.threed_ctrls_ref.cast::<HtmlDivElement>() {
+        Some(element) => element,
+        None => {
+            gloo::console::log!("No input was provided");
+            return;
+        }
+    };
     threed_controls.set_hidden(true);
     let mandelbrot_controls: HtmlDivElement =
-        match state.mandelbrot_ctrls_ref.cast::<web_sys::HtmlDivElement>() {
+        match state.mandelbrot_ctrls_ref.cast::<HtmlDivElement>() {
             Some(element) => element,
             None => {
                 gloo::console::log!("No input was provided");
@@ -153,72 +223,30 @@ fn selection_callback<T>(_: T) {
         };
     mandelbrot_controls.set_hidden(true);
 
-    match key {
-        0 | 1 | 2 | 3 | 4 | 5 => {
-            let _ = func_plot::draw(element, key);
-            return;
+    match upgrade {
+        UpgradeType::Graph(power) => {
+            let _ = func_plot::draw(canvas, power);
         }
-        6 => {
+        UpgradeType::Mandelbrot(iterations) => {
             mandelbrot_controls.set_hidden(false);
-            let iters = match state.iters_ref.cast::<web_sys::HtmlInputElement>() {
-                Some(element) => element.value_as_number(),
-                None => {
-                    gloo::console::log!("No input was provided");
-                    return;
-                }
-            };
-            let _ = mandelbrot::draw(element, iters as usize);
-            return;
+            let _ = mandelbrot::draw(canvas, iterations);
         }
-        7 => {
+        UpgradeType::Plot3d(pitch, yaw) => {
             threed_controls.set_hidden(false);
-
-            let pitch = match state.pitch_ref.cast::<web_sys::HtmlInputElement>() {
-                Some(element) => element.value_as_number(),
-                None => {
-                    gloo::console::log!("No input was provided");
-                    return;
-                }
-            };
-            let yaw = match state.yaw_ref.cast::<web_sys::HtmlInputElement>() {
-                Some(element) => element.value_as_number(),
-                None => {
-                    gloo::console::log!("No input was provided");
-                    return;
-                }
-            };
-            let _ = plot3d::draw(element, pitch, yaw);
-            return;
+            let _ = plot3d::draw(canvas, pitch, yaw);
         }
-        _ => {}
     }
-}
 
-fn update_plot_3d<T>(_: T) {
-    let state = get_state();
-
-    let canvas = match state.canvas_ref.cast::<web_sys::HtmlCanvasElement>() {
+    let end_time = performance.now();
+    let duration = end_time - start_time;
+    let status = match state.status_ref.cast::<HtmlDivElement>() {
         Some(element) => element,
         None => {
             gloo::console::log!("No input was provided");
             return;
         }
     };
-    let pitch = match state.pitch_ref.cast::<web_sys::HtmlInputElement>() {
-        Some(element) => element.value_as_number(),
-        None => {
-            gloo::console::log!("No input was provided");
-            return;
-        }
-    };
-    let yaw = match state.yaw_ref.cast::<web_sys::HtmlInputElement>() {
-        Some(element) => element.value_as_number(),
-        None => {
-            gloo::console::log!("No input was provided");
-            return;
-        }
-    };
-    let _ = plot3d::draw(canvas, pitch, yaw);
+    status.set_inner_text(&format!("Updating canvas took {}ms", duration))
 }
 
 fn main() {
@@ -235,6 +263,7 @@ fn get_state() -> State {
         mandelbrot_ctrls_ref,
         iters_ref,
         iters_label_ref,
+        status_ref,
     ) = (
         CANVAS_REF.with(|inner| inner.clone()),
         SELECT_REF.with(|inner| inner.clone()),
@@ -244,6 +273,7 @@ fn get_state() -> State {
         MANDELBROT_CTRLS_REF.with(|inner| inner.clone()),
         ITERS_REF.with(|inner| inner.clone()),
         ITERS_LABEL_REF.with(|inner| inner.clone()),
+        STATUS_REF.with(|inner| inner.clone()),
     );
     State {
         select_ref,
@@ -254,5 +284,6 @@ fn get_state() -> State {
         mandelbrot_ctrls_ref,
         iters_ref,
         iters_label_ref,
+        status_ref,
     }
 }
