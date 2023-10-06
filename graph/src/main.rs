a0bd6d0b62ab18315b1e87c602d0ac8da8d9d9b8
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2023-10-06 17:20:44 +0200

    made plotters visualization

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -1,87 +1,185 @@
-use std::{
-    collections::{HashMap, HashSet},
-    sync::{Arc, Mutex},
-};
+use std::collections::{HashMap, HashSet};
+
+use rand::Rng;
 
 use uuid::Uuid;
 
-#[derive(Hash)]
+use serde::{Deserialize, Serialize};
+
+use plotters::prelude::{
+    BitMapBackend, ChartBuilder, Circle, EmptyElement, IntoDrawingArea, LineSeries, PointSeries,
+    RGBColor, BLACK, RED, WHITE,
+};
+
+enum AddEdgeError {
+    TargetNodeMissing(Uuid),
+    SourceNodeMissing(Uuid),
+    BothNodesMissing(Uuid, Uuid),
+}
+
+impl std::fmt::Display for AddEdgeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            AddEdgeError::TargetNodeMissing(_) => write!(f, "TargetNodeMissing"),
+            AddEdgeError::SourceNodeMissing(_) => write!(f, "SourceNodeMissing"),
+            AddEdgeError::BothNodesMissing(_, _) => write!(f, "BothNodesMissing"),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct NodeMetaData {
+    position: (i32, i32),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 struct GraphNode {
-    outgoing: Vec<Uuid>,
-    incoming: Vec<Uuid>,
-    id: Uuid,
+    pub outgoing: HashSet<Uuid>,
+    pub incoming: HashSet<Uuid>,
+    pub id: Uuid,
+    meta: Option<NodeMetaData>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 struct Graph {
-    nodes: Vec<GraphNode>,
-}
+    pub nodes: Vec<GraphNode>,
 
-impl PartialEq for GraphNode {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
-    }
-}
-
-fn get_multi_mut<T>(v: &mut [T], i: usize, j: usize) -> Option<(&mut T, &mut T)> {
-    if i == j {
-        return None;
-    }
-    let (start, end) = if i < j { (i, j) } else { (j, i) };
-
-    let (first, second) = v.split_at_mut(start + 1);
-    Some((&mut first[start], &mut second[end - start - 1]))
+    /// Mapping table from a node ID to the node's index in the Nodes vector
+    pub node_lookup: HashMap<Uuid, usize>,
 }
 
 impl Graph {
-    fn add_outgoing_edge(&mut self, source_idx: Uuid, target_idx: Uuid) {
-        let (source, target) = self.get_multi_mut(source_idx, target_idx).unwrap();
-        source.outgoing.push(target.id);
-        target.incoming.push(source.id);
+    fn new() -> Self {
+        Graph {
+            nodes: Vec::new(),
+            node_lookup: HashMap::new(),
+        }
     }
-    fn add_incoming_edge(&mut self, source_idx: Uuid, target_idx: Uuid) {
-        let (source, target) = self.get_multi_mut(source_idx, target_idx).unwrap();
-        source.incoming.push(target.id);
-        target.outgoing.push(source.id);
+
+    fn add_node(&mut self) {
+        let id = Uuid::new_v4();
+        if !self.node_lookup.contains_key(&id) {
+            let node = GraphNode {
+                outgoing: HashSet::new(),
+                incoming: HashSet::new(),
+                id,
+                meta: None,
+            };
+            let index = self.nodes.len();
+            self.nodes.push(node);
+            self.node_lookup.insert(id, index);
+        }
     }
-    fn get_multi_mut(
-        &mut self,
-        source_id: Uuid,
-        target_id: Uuid,
-    ) -> Option<(&mut GraphNode, &mut GraphNode)> {
-        let source_idx = self
-            .nodes
-            .iter()
-            .enumerate()
-            .find(|(_, node)| node.id == source_id)
-            .map(|(index, _)| index);
-        let target_idx = self
-            .nodes
-            .iter()
-            .enumerate()
-            .find(|(_, node)| node.id == target_id)
-            .map(|(index, _)| index);
-        let (source_idx, target_idx) = match (source_idx, target_idx) {
-            (Some(source_idx), Some(target_idx)) => (source_idx, target_idx),
-            _ => return None,
-        };
-        get_multi_mut(self.nodes.iter_mut().into_slice(), source_idx, target_idx)
+
+    fn add_outgoing_edge(&mut self, source_id: Uuid, target_id: Uuid) -> Result<(), AddEdgeError> {
+        match (
+            self.node_lookup.get(&source_id),
+            self.node_lookup.get(&target_id),
+        ) {
+            (Some(source_idx), Some(target_idx)) => {
+                self.nodes[*source_idx].outgoing.insert(target_id);
+                self.nodes[*target_idx].incoming.insert(source_id);
+                Ok(())
+            }
+            (Some(_), None) => Err(AddEdgeError::TargetNodeMissing(target_id)),
+            (None, Some(_)) => Err(AddEdgeError::SourceNodeMissing(source_id)),
+            (None, None) => Err(AddEdgeError::BothNodesMissing(source_id, target_id)),
+        }
+    }
+
+    fn add_incoming_edge(&mut self, source_id: Uuid, target_id: Uuid) -> Result<(), AddEdgeError> {
+        self.add_outgoing_edge(target_id, source_id)
+    }
+
+    fn randomize(&mut self, num_nodes: usize, num_edges: usize) {
+        let mut rng = rand::thread_rng();
+
+        // Generate random nodes
+        for _ in 0..num_nodes {
+            self.add_node();
+        }
+
+        // Generate random edges
+        for _ in 0..num_edges {
+            let mut closure = || {
+                let source_idx = rng.gen_range(0..self.nodes.len());
+                let target_idx = rng.gen_range(0..self.nodes.len());
+
+                let source_id = self.nodes[source_idx].id;
+                let target_id = self.nodes[target_idx].id;
+
+                // Add the edge
+                if let Err(err) = self.add_outgoing_edge(source_id, target_id) {
+                    eprintln!("Error adding edge: {}", err);
+                }
+            };
+            closure()
+        }
+    }
+
+    fn new_random(num_nodes: usize, num_edges: usize) -> Self {
+        let mut graph = Self::new();
+        graph.randomize(num_nodes, num_edges);
+        graph
     }
 }
 
 fn main() {
-    // // Create some nodes
-    // let node1 = Arc::new(Mutex::new(GraphNode::new()));
-    // let node2 = Arc::new(Mutex::new(GraphNode::new()));
-    // let node3 = Arc::new(Mutex::new(GraphNode::new()));
+    let mut graph = Graph::new_random(10, 10);
 
-    // // Create edges
-    // GraphNode::add_outgoing_edge(&node1, &node2);
-    // GraphNode::add_outgoing_edge(&node2, &node3);
-    // // GraphNode::add_outgoing_edge(&node3, &node1);
+    println!(
+        "{graph_json}",
+        graph_json = serde_json::to_string_pretty(&graph).unwrap()
+    );
 
-    // println!("Node 1: {node1:?}");
-    // println!("Node 1 dfs: {dfs:?}", dfs = GraphNode::dfs(&node1));
+    visualize_graph(&mut graph);
+}
 
-    // // Perform operations on the graph
-    // // ...
+fn visualize_graph(graph: &mut Graph) -> Result<(), Box<dyn std::error::Error>> {
+    let mut rng = rand::thread_rng();
+    // Create a drawing area
+    let root = BitMapBackend::new("graph.png", (1000, 1000)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    // Create a chart context
+    let mut chart = ChartBuilder::on(&root)
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .margin(5)
+        .build_cartesian_2d(0..1000, 0..1000)?;
+
+    // Plot nodes as scatter points
+    for node in &mut graph.nodes {
+        let x = rng.gen_range(0..1000);
+        let y = rng.gen_range(0..1000);
+
+        node.meta = Some(NodeMetaData { position: (x, y) });
+
+        chart.draw_series(PointSeries::of_element(
+            vec![(x, y)],
+            5,
+            &RED,
+            &|c, s, st| {
+                return EmptyElement::at(c) // We want the point to be at (x, y)
+                        + Circle::new((0, 0), s, st.filled()); // And a circle that is 2 pixels large
+            },
+        ))?;
+    }
+
+    // Plot edges as lines
+    // It is critical that this happens after the last loop, as the last loop defined positions
+    for node in &graph.nodes {
+        let source_pos = node.meta.as_ref().unwrap().position;
+        for &outgoing_id in &node.outgoing {
+            let target_idx = graph.node_lookup.get(&outgoing_id).unwrap();
+            let target_node = graph.nodes.get(*target_idx).unwrap();
+            let target_pos = target_node.meta.as_ref().unwrap().position;
+
+            chart.draw_series(LineSeries::new(vec![source_pos, target_pos], &BLACK))?;
+        }
+    }
+
+    // Export the plot as an image
+    root.present()?;
+    Ok(())
 }
