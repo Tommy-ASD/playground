75493a2331817179475f77e670eedd75e552afad
Tommy-ASD <tommy.aleksander.sd@gmail.com>
2023-10-11 09:44:39 +0200

    modified dfs to include the path

--- graph/src/shortest_path/bfs.rs
+++ graph/src/shortest_path/bfs.rs
@@ -5,26 +5,28 @@ use uuid::Uuid;
 
 use crate::Graph;
 
-pub fn dfs(graph: &Graph, start_node_id: Uuid) -> HashMap<Uuid, i32> {
+pub fn dfs(graph: &Graph, start_node_id: Uuid) -> HashMap<Uuid, Vec<Uuid>> {
     let mut visited = HashMap::new();
-    dfs_recursive_helper(graph, start_node_id, &mut visited, 0);
+    let mut path = vec![start_node_id];
+    dfs_recursive_helper(graph, start_node_id, &mut visited, path);
     visited
 }
 
 fn dfs_recursive_helper(
     graph: &Graph,
     current_node_id: Uuid,
-    visited: &mut HashMap<Uuid, i32>,
-    depth: i32,
+    visited: &mut HashMap<Uuid, Vec<Uuid>>,
+    mut path: Vec<Uuid>,
 ) {
     if let Some(v) = visited.get(&current_node_id) {
-        if v < &depth {
+        if v.len() < path.len() {
             return;
         }
     }
-    visited.insert(current_node_id, depth);
+    visited.insert(current_node_id, path.clone());
+    path.push(current_node_id);
     graph
         .get_neighbors(current_node_id)
         .iter()
-        .for_each(|node_id| dfs_recursive_helper(graph, *node_id, visited, depth + 1));
+        .for_each(|node_id| dfs_recursive_helper(graph, *node_id, visited, path.clone()));
 }
