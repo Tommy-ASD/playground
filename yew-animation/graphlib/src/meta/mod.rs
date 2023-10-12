56e2f82e139142dfd20f77c60c0611b1b9d22121
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2023-10-12 17:02:30 +0200

    added line crossing function

--- yew-animation/graphlib/src/meta/mod.rs
+++ yew-animation/graphlib/src/meta/mod.rs
@@ -43,3 +43,29 @@ pub struct Line {
     pub from: Coordinate,
     pub to: Coordinate,
 }
+
+impl Line {
+    fn crossing(&self, other: &Self) -> bool {
+        // Calculate the direction vectors of the lines
+        let dx1 = self.to.x - self.from.x;
+        let dy1 = self.to.y - self.from.y;
+        let dx2 = other.to.x - other.from.x;
+        let dy2 = other.to.y - other.from.y;
+
+        // Calculate determinants
+        let det = dx1 * dy2 - dx2 * dy1;
+
+        // Check if the lines are not parallel and intersect
+        if det.abs() < 1e-10 {
+            return false;
+        }
+
+        // Calculate the parameters for the intersection point
+        let t1 = ((other.from.x - self.from.x) * dy2 - (other.from.y - self.from.y) * dx2) / det;
+        let t2 = ((other.from.x - self.from.x) * dy1 - (other.from.y - self.from.y) * dx1) / det;
+
+        // Check if the intersection point is within the lines
+        (OrderedFloat(0.0) <= t1 && t1 <= OrderedFloat(1.0))
+            && (OrderedFloat(0.0) <= t2 && t2 <= OrderedFloat(1.0))
+    }
+}
