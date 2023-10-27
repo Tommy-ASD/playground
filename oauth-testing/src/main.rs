d24255e8fe7e9b08a7c385b09951630fb54d5da2
Tommy-ASD <tommy.aleksander.sd@gmail.com>
2023-10-27 15:34:41 +0200

    generalizing token providers

--- oauth-testing/src/main.rs
+++ oauth-testing/src/main.rs
@@ -13,6 +13,8 @@
 //! ...and follow the instructions.
 //!
 
+use chrono::{Duration, Utc};
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use oauth2::{
     basic::{BasicClient, BasicErrorResponseType, BasicTokenType},
     reqwest::{async_http_client, Error as OAuthError},
@@ -23,6 +25,7 @@ use oauth2::{
 };
 
 use reqwest::Error as ReqwestError;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use traceback_error::{traceback, TracebackError};
 
 use std::env;
@@ -50,16 +53,14 @@ type TokenResult = Result<
 >;
 type Token = StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>;
 
-#[macro_use]
-extern crate dotenv_codegen;
+use dotenv_codegen::dotenv;
 
-#[tokio::main]
-async fn main() {
-    dotenv::dotenv().ok();
-    discord_oauth(&vec![Scope::new("identify".to_string())])
-        .await
-        .unwrap();
-}
+use crate::types::{DiscordIdentity, GithubIdentity, UserType, JWT};
+
+const PRIVATE_KEY: &[u8] = include_bytes!("../private_key.pem");
+const PUBLIC_KEY: &[u8] = include_bytes!("../public_key.pem");
+
+pub mod types;
 
 #[derive(Clone, Debug)]
 pub struct OAuth2Info {
@@ -68,6 +69,70 @@ pub struct OAuth2Info {
     pub auth_url: String,
     pub token_url: String,
     pub redirect_url: String,
+}
+
+impl OAuth2Info {
+    fn github_default() -> Self {
+        let client_id = dotenv!("GITHUB_CLIENT_ID");
+        let client_secret = dotenv!("GITHUB_CLIENT_SECRET");
+        let auth_url = "https://github.com/login/oauth/authorize".to_string();
+        let token_url = "https://github.com/login/oauth/access_token".to_string();
+        OAuth2Info {
+            client_id: client_id.to_string(),
+            client_secret: client_secret.to_string(),
+            auth_url,
+            token_url,
+            redirect_url: "http://localhost:8080/oauth_callback".to_string(),
+        }
+    }
+    fn discord_default() -> Self {
+        let client_id = dotenv!("DISCORD_CLIENT_ID");
+        let client_secret = dotenv!("DISCORD_CLIENT_SECRET");
+        let auth_url = "https://discord.com/api/oauth2/authorize".to_string();
+        let token_url = "https://discord.com/api/oauth2/token".to_string();
+
+        OAuth2Info {
+            client_id: client_id.to_string(),
+            client_secret: client_secret.to_string(),
+            auth_url,
+            token_url,
+            redirect_url: "http://localhost:49103/oauth/callback/discord".to_string(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+#[allow(dead_code)]
+enum OAuthType {
+    Standard,
+    Github,
+}
+
+#[derive(Clone, Debug)]
+struct OAuthParams {
+    pub provider_name: String,
+    pub general_info: OAuth2Info,
+    pub scopes: Vec<Scope>,
+    pub oauth_type: OAuthType,
+}
+
+impl OAuthParams {
+    fn discord_default() -> Self {
+        OAuthParams {
+            provider_name: "Discord".to_string(),
+            general_info: OAuth2Info::discord_default(),
+            scopes: vec![Scope::new("identify".to_string())],
+            oauth_type: OAuthType::Standard,
+        }
+    }
+    fn github_default() -> Self {
+        OAuthParams {
+            provider_name: "Github".to_string(),
+            general_info: OAuth2Info::github_default(),
+            scopes: vec![],
+            oauth_type: OAuthType::Github,
+        }
+    }
 }
 
 fn build_client(info: OAuth2Info) -> Result<ClientWithGenerics, TracebackError> {
@@ -90,22 +155,8 @@ fn build_client(info: OAuth2Info) -> Result<ClientWithGenerics, TracebackError> 
     Ok(client)
 }
 
-#[derive(Clone, Debug)]
-#[allow(dead_code)]
-enum OAuthType {
-    Standard,
-    Github,
-}
-
-#[derive(Clone, Debug)]
-struct OAuthParams {
-    pub provider_name: String,
-    pub general_info: OAuth2Info,
-    pub scopes: Vec<Scope>,
-    pub oauth_type: OAuthType,
-}
-
-async fn oauth(params: OAuthParams) -> Result<(), TracebackError> {
+#[traceback_derive::traceback]
+async fn oauth(params: OAuthParams) -> Result<String, TracebackError> {
     let client = match build_client(params.general_info.clone()) {
         Ok(client) => client,
         Err(e) => return Err(traceback!(err e, "Failed to build client")),
@@ -122,16 +173,15 @@ async fn oauth(params: OAuthParams) -> Result<(), TracebackError> {
         "\nOpen this URL in your browser:\n{}\n",
         authorize_url.to_string()
     );
-    tcp_server(client, csrf_state, params).await?;
-
-    Ok(())
+    Ok(tcp_server(client, csrf_state, params).await?)
 }
 
+#[traceback_derive::traceback]
 async fn tcp_server(
     client: ClientWithGenerics,
     csrf_state: CsrfToken,
     params: OAuthParams,
-) -> Result<(), TracebackError> {
+) -> Result<String, TracebackError> {
     // A very naive implementation of the redirect server.
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     loop {
@@ -160,73 +210,21 @@ async fn tcp_server(
                 .await;
 
             println!(
-                "{provider_name} returned the following token:\n{:?}\n",
-                token_res,
+                "{provider_name} returned the following token:\n{token_res:?}\n",
                 provider_name = params.provider_name
             );
-            match params.oauth_type {
+            let token = match params.oauth_type {
                 OAuthType::Standard => handle_standard_token(token_res)?,
                 OAuthType::Github => handle_github_token(token_res)?,
             };
 
             // The server will terminate itself after collecting the first code.
-            return Ok(());
+            return Ok(token);
         }
     }
 }
 
-async fn discord_oauth(scopes: &Vec<Scope>) -> Result<(), TracebackError> {
-    let github_client_id = dotenv!("DISCORD_CLIENT_ID");
-    let github_client_secret = dotenv!("DISCORD_CLIENT_SECRET");
-    let auth_url = "https://discord.com/api/oauth2/authorize".to_string();
-    let token_url = "https://discord.com/api/oauth2/token".to_string();
-
-    let info = OAuth2Info {
-        client_id: github_client_id.to_string(),
-        client_secret: github_client_secret.to_string(),
-        auth_url,
-        token_url,
-        redirect_url: "http://localhost:8080/oauth_callback".to_string(),
-    };
-
-    let params = OAuthParams {
-        provider_name: "Discord".to_string(),
-        general_info: info,
-        scopes: scopes.clone(),
-        oauth_type: OAuthType::Standard,
-    };
-
-    oauth(params).await?;
-
-    Ok(())
-}
-
-async fn github_oauth(scopes: &Vec<Scope>) -> Result<(), TracebackError> {
-    let github_client_id = dotenv!("GITHUB_CLIENT_ID");
-    let github_client_secret = dotenv!("GITHUB_CLIENT_SECRET");
-    let auth_url = "https://github.com/login/oauth/authorize".to_string();
-    let token_url = "https://github.com/login/oauth/access_token".to_string();
-
-    let info = OAuth2Info {
-        client_id: github_client_id.to_string(),
-        client_secret: github_client_secret.to_string(),
-        auth_url,
-        token_url,
-        redirect_url: "http://localhost:8080/oauth_callback".to_string(),
-    };
-
-    let params = OAuthParams {
-        provider_name: "Github".to_string(),
-        general_info: info,
-        scopes: scopes.clone(),
-        oauth_type: OAuthType::Github,
-    };
-
-    oauth(params).await?;
-
-    Ok(())
-}
-
+#[traceback_derive::traceback]
 async fn parse_oauth_callback_request(
     stream: &mut TcpStream,
 ) -> Result<(AuthorizationCode, CsrfToken), TracebackError> {
@@ -237,6 +235,8 @@ async fn parse_oauth_callback_request(
     // Reads until a newline is encountered (which is discarded).
     // Result should be "GET /oauth_callback?code={code} HTTP/1.1\r\n".
     reader.read_line(&mut request_line).await.unwrap();
+
+    println!("Line: {request_line}");
 
     // With that, we can get the 2nd element of the request_line split by whitespace.
     // It should be "/oauth_callback?code={code}".
@@ -273,6 +273,7 @@ async fn parse_oauth_callback_request(
     Ok((code, state))
 }
 
+#[traceback_derive::traceback]
 async fn respond_to_oauth_callback(stream: &mut TcpStream) -> Result<(), TracebackError> {
     // Respond with a success code.
     let message = "Go back to your terminal :)";
@@ -289,37 +290,112 @@ async fn respond_to_oauth_callback(stream: &mut TcpStream) -> Result<(), Traceba
     }
 }
 
-fn handle_github_token(token_res: TokenResult) -> Result<(), TracebackError> {
-    if let Ok(token) = token_res {
-        // NB: Github returns a single comma-separated "scope" parameter instead of multiple
-        // space-separated scopes. Github-specific clients can parse this scope into
-        // multiple scopes by splitting at the commas. Note that it's not safe for the
-        // library to do this by default because RFC 6749 allows scopes to contain commas.
-        let scopes = if let Some(scopes_vec) = token.scopes() {
-            scopes_vec
-                .iter()
-                .map(|comma_separated| comma_separated.split(','))
-                .flatten()
-                .collect::<Vec<_>>()
-        } else {
-            Vec::new()
-        };
-        println!("Github returned the following scopes:\n{:?}\n", scopes);
-        println!("Token: {}", token.access_token().secret())
-    } else if let Err(e) = token_res {
-        let e = e;
-        println!("Token exchange failed: {}", e);
+#[traceback_derive::traceback]
+fn handle_github_token(token_res: TokenResult) -> Result<String, TracebackError> {
+    match token_res {
+        Ok(token) => {
+            // NB: Github returns a single comma-separated "scope" parameter instead of multiple
+            // space-separated scopes. Github-specific clients can parse this scope into
+            // multiple scopes by splitting at the commas. Note that it's not safe for the
+            // library to do this by default because RFC 6749 allows scopes to contain commas.
+            let scopes = if let Some(scopes_vec) = token.scopes() {
+                scopes_vec
+                    .iter()
+                    .map(|comma_separated| comma_separated.split(','))
+                    .flatten()
+                    .collect::<Vec<_>>()
+            } else {
+                Vec::new()
+            };
+            println!("Github returned the following scopes:\n{:?}\n", scopes);
+            Ok(token.access_token().secret().to_string())
+        }
+        Err(e) => Err(traceback!(err e)),
     }
-    Ok(())
 }
 
-fn handle_standard_token(token_res: TokenResult) -> Result<(), TracebackError> {
-    if let Ok(token) = token_res {
-        let access_token = token.access_token().secret();
-        println!("Token: {}", access_token);
-    } else if let Err(e) = token_res {
-        let e = e;
-        println!("Token exchange failed: {}", e);
+#[traceback_derive::traceback]
+fn handle_standard_token(token_res: TokenResult) -> Result<String, TracebackError> {
+    match token_res {
+        Ok(token) => Ok(token.access_token().secret().to_string()),
+        Err(e) => Err(traceback!(err e)),
     }
-    Ok(())
+}
+
+#[traceback_derive::traceback]
+async fn get_discord_info(token: &str) -> Result<UserType, TracebackError> {
+    Ok(get_info::<DiscordIdentity>(token, "https://discord.com/api/v10/users/@me").await?)
+}
+
+#[traceback_derive::traceback]
+async fn get_github_info(token: &str) -> Result<UserType, TracebackError> {
+    Ok(get_info::<GithubIdentity>(token, "https://api.github.com/user").await?)
+}
+
+#[traceback_derive::traceback]
+async fn get_info<T>(token: &str, url: &str) -> Result<UserType, TracebackError>
+where
+    T: Into<UserType> + DeserializeOwned,
+{
+    let client = reqwest::Client::new();
+    let sent = match client
+        .get(url)
+        .bearer_auth(token)
+        .header("User-Agent", "reqwest")
+        .send()
+        .await
+    {
+        Ok(sent) => sent,
+        Err(e) => return Err(traceback!(err e)),
+    };
+    let response = sent.text().await?;
+    println!("Returned {response}");
+    let parsed: GithubIdentity = serde_json::from_str(&response)?;
+    Ok(parsed.into())
+}
+
+#[tokio::main]
+async fn main() {
+    let token = oauth(OAuthParams::discord_default()).await.unwrap();
+    let user = get_discord_info(&token).await.unwrap();
+    let current_time = Utc::now();
+    let expiration_time = current_time + Duration::days(30);
+
+    let jwt = JWT {
+        user,
+        iat: current_time.timestamp(),
+        exp: expiration_time.timestamp(),
+    };
+
+    let algorithm = Algorithm::RS512;
+
+    let token = encode(
+        &Header::new(algorithm),
+        &jwt,
+        &EncodingKey::from_rsa_pem(PRIVATE_KEY).unwrap(),
+    );
+
+    match token {
+        Ok(ok_jwt) => {
+            println!("JWT: {}", ok_jwt);
+
+            // Define the expected JWT algorithm and validation settings
+            let validation = Validation::new(algorithm);
+
+            // Decode and validate the JWT
+            match decode::<JWT>(
+                &ok_jwt,
+                &DecodingKey::from_rsa_pem(PUBLIC_KEY).unwrap(),
+                &validation,
+            ) {
+                Ok(token_data) => {
+                    println!("Valid JWT. Claims: {:?}", token_data.claims);
+                }
+                Err(e) => {
+                    eprintln!("Invalid JWT: {}", e);
+                }
+            }
+        }
+        Err(e) => eprintln!("Failed to create JWT: {}", e),
+    }
 }
