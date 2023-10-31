0716daa7d18b40399a01f565683fbc338be8e607
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2023-10-31 15:56:41 +0100

    forgor

--- fullstack/common/src/types/mod.rs
+++ fullstack/common/src/types/mod.rs
@@ -1,4 +1,15 @@
+use std::collections::HashMap;
+
 mod messages;
 pub use messages::*;
 mod channels;
 pub use channels::*;
+use uuid::Uuid;
+
+pub struct State {
+    pub messages: Vec<Message>,
+    pub channels: Vec<Channel>,
+
+    pub message_lookup: HashMap<Uuid, Message>,
+    pub channel_lookup: HashMap<Uuid, Channel>,
+}
