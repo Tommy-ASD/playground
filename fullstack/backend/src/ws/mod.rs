d70cbe5fa00ca8271f52d4813f2f6fd89619a2a8
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2023-12-15 16:19:07 +0100

    modularizing backend functions

--- fullstack/backend/src/ws/mod.rs
+++ fullstack/backend/src/ws/mod.rs
@@ -6,7 +6,10 @@ use axum::{
     },
     response::IntoResponse,
 };
-use futures::{sink::SinkExt, stream::StreamExt};
+use futures::{
+    sink::SinkExt,
+    stream::{SplitStream, StreamExt},
+};
 use std::sync::Arc;
 
 use common::{Payload, PayloadInner};
@@ -23,57 +26,66 @@ pub async fn websocket_handler(
 // receiving / sending chat messages).
 async fn websocket(stream: WebSocket, state: Arc<AppState>) {
     // By splitting, we can send and receive at the same time.
-    let (mut sender, mut receiver) = stream.split();
+    // "sender" sends messages to the user
+    // "reciever" recieves messages from the user
+    let (mut sender, receiver) = stream.split();
 
-    // We subscribe *before* sending the "joined" message, so that we will also
-    // display it to our client.
-    let mut rx = state.tx.subscribe();
-
-    let pls = Payload::new(PayloadInner::PayloadList(state.get_payload_list().unwrap()));
+    let plst = Payload::new(PayloadInner::PayloadList(state.get_payload_list().unwrap()));
 
     sender
-        .send(ws::Message::Text(serde_json::to_string(&pls).unwrap()))
+        .send(ws::Message::Text(serde_json::to_string(&plst).unwrap()))
         .await
         .unwrap();
 
+    let state_clone = state.clone();
+
     // Spawn the first task that will receive broadcast messages and send text
     // messages over the websocket to our client.
-    let mut send_task = tokio::spawn(async move {
-        while let Ok(msg) = rx.recv().await {
-            // In any websocket error, break loop.
-            if sender
-                .send(ws::Message::Text(serde_json::to_string(&msg).unwrap()))
-                .await
-                .is_err()
-            {
-                break;
-            }
-        }
-    });
+    let mut send_task = tokio::spawn(ws_send_task(sender, state_clone));
 
     let state_clone = state.clone();
 
     // Spawn a task that takes messages from the websocket, prepends the user
     // name, and sends them to all broadcast subscribers.
-    let mut recv_task = tokio::spawn(async move {
-        while let Some(Ok(ws::Message::Text(text))) = receiver.next().await {
-            tracing::debug!("Recieved message {text}");
-            let parsed: Payload = match serde_json::from_str(&text) {
-                Ok(p) => p,
-                Err(e) => {
-                    println!("Failed to parse payload: {e}");
-                    continue;
-                }
-            };
-            tracing::debug!("Parsed message {parsed:?}");
-            // Add username before message.
-            let _ = state_clone.send(parsed);
-        }
-    });
+    let mut recv_task = tokio::spawn(ws_recv_task(receiver, state_clone));
 
     // If any one of the tasks run to completion, we abort the other.
     tokio::select! {
         _ = (&mut send_task) => recv_task.abort(),
         _ = (&mut recv_task) => send_task.abort(),
     };
+}
+
+async fn ws_recv_task(mut receiver: SplitStream<WebSocket>, state: Arc<AppState>) {
+    while let Some(Ok(ws::Message::Text(text))) = receiver.next().await {
+        tracing::debug!("Recieved message {text}");
+        let parsed: Payload = match serde_json::from_str(&text) {
+            Ok(p) => p,
+            Err(e) => {
+                println!("Failed to parse payload: {e}");
+                continue;
+            }
+        };
+        tracing::debug!("Parsed message {parsed:?}");
+        // Add username before message.
+        let _ = state.send(parsed);
+    }
+}
+
+async fn ws_send_task(
+    mut sender: futures::prelude::stream::SplitSink<WebSocket, ws::Message>,
+    state: Arc<AppState>,
+) {
+    let mut rx: tokio::sync::broadcast::Receiver<Payload> = state.tx.subscribe();
+
+    while let Ok(msg) = rx.recv().await {
+        // In any websocket error, break loop.
+        if sender
+            .send(ws::Message::Text(serde_json::to_string(&msg).unwrap()))
+            .await
+            .is_err()
+        {
+            break;
+        }
+    }
 }
