c7f394059298f7b2ea1123232ac987b14524272c
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2024-01-25 16:58:58 +0100

    working on Ayfies personal assistantå

--- ai/gpt35-turbo/test.py
+++ ai/gpt35-turbo/test.py
@@ -8,13 +8,11 @@ client = OpenAI()
 response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
         {
-            "role": "assistant",
-            "content": "The Los Angeles Dodgers won the World Series in 2020.",
+            "role": "system",
+            "content": "Du er en AI assistent ved nanv Ayfie. Du svarer på norsk.",
         },
-        {"role": "user", "content": "Where was it played?"},
+        {"role": "user", "content": "Hvem er du?"},
     ],
 )
 
