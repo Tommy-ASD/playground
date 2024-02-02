c1fec62e8f8b129eaa2d98e3ad354b786d018944
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2024-02-02 13:32:10 +0100

    poise :D

--- discord/commands/src/main.rs
+++ discord/commands/src/main.rs
@@ -1,7 +1,15 @@
 use poise::serenity_prelude as serenity;
 
-struct Data {} // User data, which is stored and accessible in all command invocations
+use std::env::var;
+use std::sync::atomic::{AtomicU32, Ordering};
+
+// Custom user data passed to all command functions
+pub struct Data {
+    poise_mentions: AtomicU32,
+}
+
 type Error = Box<dyn std::error::Error + Send + Sync>;
+#[allow(unused)]
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Displays your or another user's account creation date
@@ -12,25 +20,30 @@ async fn age(
 ) -> Result<(), Error> {
     let u = user.as_ref().unwrap_or_else(|| ctx.author());
     let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
+    ctx.say(response).await.unwrap();
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
-    let intents = serenity::GatewayIntents::non_privileged();
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
 
     let framework = poise::Framework::builder()
+        .setup(move |_ctx, _ready, _framework| {
+            Box::pin(async move {
+                Ok(Data {
+                    poise_mentions: AtomicU32::new(0),
+                })
+            })
+        })
         .options(poise::FrameworkOptions {
+            event_handler: |ctx, event, framework, data| {
+                Box::pin(event_handler(ctx, event, framework, data))
+            },
             commands: vec![age()],
             ..Default::default()
-        })
-        .setup(|ctx, _ready, framework| {
-            Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
-            })
         })
         .build();
 
@@ -38,4 +51,29 @@ async fn main() {
         .framework(framework)
         .await;
     client.unwrap().start().await.unwrap();
+}
+
+async fn event_handler(
+    ctx: &serenity::Context,
+    event: &serenity::FullEvent,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    data: &Data,
+) -> Result<(), Error> {
+    match event {
+        serenity::FullEvent::Ready { data_about_bot } => {
+            println!("Logged in as {}", data_about_bot.user.name);
+        }
+        serenity::FullEvent::Message { new_message } => {
+            // let Message { content, .. } = new_message;
+            if new_message.content.to_lowercase().contains("poise") {
+                let mentions = data.poise_mentions.load(Ordering::SeqCst) + 1;
+                data.poise_mentions.store(mentions, Ordering::SeqCst);
+                new_message
+                    .reply(ctx, format!("Poise has been mentioned {} times", mentions))
+                    .await?;
+            }
+        }
+        _ => {}
+    }
+    Ok(())
 }
