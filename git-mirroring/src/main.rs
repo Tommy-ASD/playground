466bc96fa576cab7395d5b785df4d15e78647820
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2024-02-08 12:54:38 +0100

    removed panics

--- git-mirroring/src/main.rs
+++ git-mirroring/src/main.rs
@@ -82,7 +82,10 @@ fn do_fetch<'a>(
     // Always fetch all tags.
     // Perform a download and also update tips
     fo.download_tags(git2::AutotagOption::All);
-    println!("Fetching {} for repo {name}", remote.name().unwrap());
+    println!(
+        "Fetching {} for repo {name}",
+        remote.name().unwrap_or("{REPO NAME FAILED}")
+    );
     remote.fetch(refs, Some(&mut fo), None)?;
 
     // If there are local objects (we got a thin pack), then tell the user
@@ -121,16 +124,15 @@ fn fast_forward(
     };
     let msg = format!("Fast-Forward: Setting {} to id: {}", name, rc.id());
     println!("{}", msg);
-    lb.set_target(rc.id(), &msg).unwrap();
-    repo.set_head(&name).unwrap();
+    lb.set_target(rc.id(), &msg)?;
+    repo.set_head(&name)?;
     repo.checkout_head(Some(
         git2::build::CheckoutBuilder::default()
             // For some reason the force is required to make the working directory actually get updated
             // I suspect we should be adding some logic to handle dirty working directory states
             // but this is just an example so maybe not.
             .force(),
-    ))
-    .unwrap();
+    ))?;
     Ok(())
 }
 
@@ -139,41 +141,35 @@ fn normal_merge(
     local: &git2::AnnotatedCommit,
     remote: &git2::AnnotatedCommit,
 ) -> Result<(), git2::Error> {
-    let local_tree = repo.find_commit(local.id()).unwrap().tree().unwrap();
-    let remote_tree = repo.find_commit(remote.id()).unwrap().tree().unwrap();
+    let local_tree = repo.find_commit(local.id())?.tree()?;
+    let remote_tree = repo.find_commit(remote.id())?.tree()?;
     let ancestor = repo
-        .find_commit(repo.merge_base(local.id(), remote.id()).unwrap())
-        .unwrap()
-        .tree()
-        .unwrap();
-    let mut idx = repo
-        .merge_trees(&ancestor, &local_tree, &remote_tree, None)
-        .unwrap();
+        .find_commit(repo.merge_base(local.id(), remote.id())?)?
+        .tree()?;
+    let mut idx = repo.merge_trees(&ancestor, &local_tree, &remote_tree, None)?;
 
     if idx.has_conflicts() {
         println!("Merge conflicts detected...");
-        repo.checkout_index(Some(&mut idx), None).unwrap();
+        repo.checkout_index(Some(&mut idx), None)?;
         return Ok(());
     }
-    let result_tree = repo.find_tree(idx.write_tree_to(repo).unwrap()).unwrap();
+    let result_tree = repo.find_tree(idx.write_tree_to(repo)?)?;
     // now create the merge commit
     let msg = format!("Merge: {} into {}", remote.id(), local.id());
-    let sig = repo.signature().unwrap();
-    let local_commit = repo.find_commit(local.id()).unwrap();
-    let remote_commit = repo.find_commit(remote.id()).unwrap();
+    let sig = repo.signature()?;
+    let local_commit = repo.find_commit(local.id())?;
+    let remote_commit = repo.find_commit(remote.id())?;
     // Do our merge commit and set current branch head to that commit.
-    let _merge_commit = repo
-        .commit(
-            Some("HEAD"),
-            &sig,
-            &sig,
-            &msg,
-            &result_tree,
-            &[&local_commit, &remote_commit],
-        )
-        .unwrap();
+    let _merge_commit = repo.commit(
+        Some("HEAD"),
+        &sig,
+        &sig,
+        &msg,
+        &result_tree,
+        &[&local_commit, &remote_commit],
+    )?;
     // Set working tree to match head.
-    repo.checkout_head(None).unwrap();
+    repo.checkout_head(None)?;
     Ok(())
 }
 
@@ -181,9 +177,9 @@ fn do_merge<'a>(
     repo: &'a Repository,
     remote_branch: &str,
     fetch_commit: git2::AnnotatedCommit<'a>,
-) {
+) -> Result<(), git2::Error> {
     // 1. do a merge analysis
-    let analysis = repo.merge_analysis(&[&fetch_commit]).unwrap();
+    let analysis = repo.merge_analysis(&[&fetch_commit])?;
 
     // 2. Do the appropriate merge
     if analysis.0.is_fast_forward() {
@@ -192,7 +188,7 @@ fn do_merge<'a>(
         let refname = format!("refs/heads/{}", remote_branch);
         match repo.find_reference(&refname) {
             Ok(mut r) => {
-                fast_forward(repo, &mut r, &fetch_commit).unwrap();
+                fast_forward(repo, &mut r, &fetch_commit)?;
             }
             Err(_) => {
                 // The branch doesn't exist so just set the reference to the
@@ -203,27 +199,24 @@ fn do_merge<'a>(
                     fetch_commit.id(),
                     true,
                     &format!("Setting {} to {}", remote_branch, fetch_commit.id()),
-                )
-                .unwrap();
-                repo.set_head(&refname).unwrap();
+                )?;
+                repo.set_head(&refname)?;
                 repo.checkout_head(Some(
                     git2::build::CheckoutBuilder::default()
                         .allow_conflicts(true)
                         .conflict_style_merge(true)
                         .force(),
-                ))
-                .unwrap();
+                ))?;
             }
         };
     } else if analysis.0.is_normal() {
         // do a normal merge
-        let head_commit = repo
-            .reference_to_annotated_commit(&repo.head().unwrap())
-            .unwrap();
-        normal_merge(&repo, &head_commit, &fetch_commit).unwrap();
+        let head_commit = repo.reference_to_annotated_commit(&repo.head()?)?;
+        normal_merge(&repo, &head_commit, &fetch_commit)?;
     } else {
         // println!("Nothing to do...");
     }
+    Ok(())
 }
 
 fn run_inner(
@@ -234,7 +227,7 @@ fn run_inner(
 ) -> Result<(), git2::Error> {
     let mut remote = repo.find_remote(remote_name)?;
     let fetch_commit = do_fetch(name, &repo, &[remote_branch], &mut remote)?;
-    Ok(do_merge(&repo, &remote_branch, fetch_commit))
+    Ok(do_merge(&repo, &remote_branch, fetch_commit)?)
 }
 
 #[tokio::main]
