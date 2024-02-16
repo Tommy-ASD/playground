fb34d178b8ae64b6aed3535db752d4ccbd51b956
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2024-02-16 10:35:37 +0100

    moved fullstack to another repo

--- fullstack/backend/src/api/mod.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::Arc;
-
-use axum::Router;
-
-use crate::AppState;
-
-pub fn make_router() -> Router<Arc<AppState>> {
-    Router::new()
-}
--- fullstack/common/src/types/channels.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug)]
-pub struct Channel {
-    id: Uuid,
-    permitted_users: Vec<Uuid>,
-    messages: Vec<Uuid>,
-}
