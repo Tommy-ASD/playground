a002487a60948c9f409babc273bb9dec56d88474
Tommy-ASD <tommy.aleksander.sd@gmail.cm>
2026-02-03 11:54:31 +0100

    im deleting a bunch of stuff from the repo because i know its all still in history and i need t oclean this up

--- brainfrick/src/main.rs
+++
@@ -1,9 +0,0 @@
-use brainfuck::context::Context;
-use brainfuck::parser;
-
-fn main() {
-    let hello_world = include_bytes!("hello_world.bf");
-    let mut ctx = Context::new();
-    let block = parser::parse(&hello_world[..]).expect("Failed parsing input file");
-    ctx.run(&block);
-}
